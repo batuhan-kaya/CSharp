9fb1c6ee72368f9d48c4ea8622a239a34657fbac
Batuhan KAYA <50137562+batuhan-kaya@users.noreply.github.com>
2021-02-04 23:25:45 +0300

    Sayı bilme oyunu
    C# dili ile kodladığım, oyun 0 ile 100 arasındaki rastgele sayıyı bulması için son kullanıcıya 10 hak tanıyor. 
    
    Her tahmin sonrası sayının daha büyük ya da daha küçük olduğuna dair direktifler yardımı mevcut. 
    
    Tahmin sonrası kalan hakkımızı görebiliyoruz. 
    
    Hatalı sayı girildiğinde hakkımızın boşa gitmiyor.

---
+++ numbersGame/numbersGame/numbersGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numbersGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Random = new Random();
+            int randomNumber = Random.Next(0, 100);
+            for (int i = 9; i >=0; i--)
+            {
+                Console.WriteLine("0-100 Arası sayı giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                if (sayi<0 || sayi>=101)
+
+                {
+                    i++;
+                }
+                else if(sayi == randomNumber)
+                {
+                    Console.WriteLine("Tebrikler bildiniz. Sayı = {0}", randomNumber);
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+                else if(sayi > randomNumber)
+                {
+                    Console.WriteLine("Sayı daha küçüktür, kalan hakkınız = {0}", i);
+                }
+                else
+                {
+                    Console.WriteLine("Sayı daha büyüktür, kalan hakkınız = {0}", i);
+                }
+            }
+            Console.Write("Hakkınız bitti.");
+            Console.ReadLine();
+        }
+    }
+}
